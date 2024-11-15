7d912592a1816e20a16b853fc60db2add97dfbe4
lajord <79750507+lajord@users.noreply.github.com>
2024-11-15 12:18:03 +0100

    Add files via upload
    Update MainController

--- gestionlocation/src/main/java/com/agencelocation/controller/MainController.java
+++ gestionlocation/src/main/java/com/agencelocation/controller/MainController.java
@@ -1,1 +1,19 @@
+package com.agencelocation.controller;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")  // Route pour la page d'accueil
+    public String home() {
+        return "index"; // Correspond au fichier index.html
+    }
+
+
+}

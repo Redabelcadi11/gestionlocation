2088304375b7c3a646657dd3530f5433f5ba34a4
lajord <79750507+lajord@users.noreply.github.com>
2024-11-23 14:16:59 +0100

    Add files via upload
    Update des repository

--- gestionlocation/src/main/java/com/agencelocation/repository/ClientRepository.java
+++ gestionlocation/src/main/java/com/agencelocation/repository/ClientRepository.java
@@ -2,9 +2,10 @@ package com.agencelocation.repository;
 
 import com.agencelocation.model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+import java.util.Optional;
+
 public interface ClientRepository extends JpaRepository<Client, Long> {
-    // Ajoute des méthodes de requêtes personnalisées si nécessaire
+    Optional<Client> findByUsername(String username);  // Trouver un client par son nom d'utilisateur
+    Optional<Client> findByEmail(String email);        // Trouver un client par son email
 }
---
+++ gestionlocation/src/main/java/com/agencelocation/repository/PaiementRepository.java
@@ -0,0 +1,4 @@
+package com.agencelocation.repository;
+
+public interface PaiementRepository {
+}
